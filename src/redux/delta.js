dcad1c9d83d194d3ea475fe9f6494338f00c91b5
Richard Foster <richard@madetech.co.uk>
2016-07-26 09:40:15 +0100

    Support text insertion and backwards cursors

--- src/redux/delta.js
+++ src/redux/delta.js
@@ -12,12 +12,16 @@ const SET_CURSOR = Symbol()
 const SET_SELECTION = Symbol()
 const SET_SELECTION_ATTRIBUTE = Symbol()
 const DELETE_SELECTION = Symbol()
+const INSERT = Symbol()
+const INSERT_LINEBREAK = Symbol()
 
 export const setDelta = createAction(SET_DELTA)
 export const setCursor = createAction(SET_CURSOR)
 export const setSelection = createAction(SET_SELECTION)
 export const setSelectionAttribute = createAction(SET_SELECTION_ATTRIBUTE)
 export const deleteSelection = createAction(DELETE_SELECTION)
+export const insert = createAction(INSERT)
+export const insertLinebreak = createAction(INSERT_LINEBREAK)
 
 const handlers = {
   [SET_DELTA] (state, { payload }) {
@@ -25,7 +29,8 @@ const handlers = {
   },
 
   [SET_CURSOR] (state, { payload }) {
-    return { ...state, cursor: payload, selectionLength: null }
+    const cursor = payload > 0 && payload || state.delta.length() - payload
+    return { ...state, cursor, selectionLength: null }
   },
 
   [SET_SELECTION] (state, { payload }) {
@@ -41,6 +46,18 @@ const handlers = {
     const update = new Delta().retain(state.cursor).delete(payload.length)
     return { ...state, delta: state.delta.compose(update) }
   },
+
+  [INSERT] (state, { payload }) {
+    const { content, attributes = {} } = payload
+    const update = new Delta().retain(state.cursor).insert(content, attributes)
+    return { ...state, cursor: state.cursor + content.length, delta: state.delta.compose(update) }
+  },
+
+  [INSERT_LINEBREAK] (state) {
+    const content = '\n'
+    const update = new Delta().retain(state.cursor).insert(content, { linebreak: true })
+    return { ...state, cursor: state.cursor + content.length, delta: state.delta.compose(update) }
+  }
 }
 
 export const reducer = handleActions(handlers, INITIAL_STATE)
