9f85f1134e343f7f899b0279cf371277967bf923
Richard Foster <richard@madetech.co.uk>
2016-07-26 11:52:00 +0100

    Cleanup JSX encoder

--- src/encoders/jsx.js
+++ src/encoders/jsx.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-const element = tag => (content, attributes) => React.createElement(tag, attributes, content)
-const formatters = {
-  bold: element('strong'),
-  italic: element('em'),
-  link: element('a')
+const factories = {
+  bold: React.createFactory('strong'),
+  italic: React.createFactory('em'),
+  link: React.createFactory('a')
 }
 
-function paragraphs (operations) {
+function operationParagraphs (operations) {
   return operations.reduce((acc, operation) => {
     if (operation.attributes && operation.attributes.linebreak) {
       return [...acc, []]
@@ -21,26 +20,28 @@ function paragraphs (operations) {
   }, [[]])
 }
 
+function renderInsertion (operation) {
+  const keys = Object.keys(operation.attributes || {})
+  const factoryNames = keys.filter(key => !!factories[key])
+
+  return factoryNames.reduce((element, name) => {
+    const factory = factories[name]
+    const factoryOptions = operation.attributes[name]
+    const props = Object(factoryOptions) === factoryOptions ? factoryOptions : {}
+
+    return factory(props, element)
+  }, operation.insert)
+}
+
 function paragraph (operations) {
   const insertions = operations.filter(op => op.hasOwnProperty('insert'))
-  const formatted = insertions.map(operation => {
-    const keys = Object.keys(operation.attributes || {})
-    const formatterNames = keys.filter(key => !!formatters[key])
+  const children = insertions.map(renderInsertion)
 
-    return formatterNames.reduce((acc, name) => {
-      const formatter = formatters[name]
-      const formatterOptions = operation.attributes[name]
-      const props = Object(formatterOptions) === formatterOptions ? formatterOptions : {}
-
-      return formatter(acc, props)
-    }, operation.insert)
-  })
-
-  return <p>{formatted}</p>
+  return <p>{children}</p>
 }
 
 export default function encodeJSX (delta) {
-  const children = paragraphs(delta.ops).map(paragraph)
+  const children = operationParagraphs(delta.ops).map(paragraph)
 
   return <section>{children}</section>
 }
